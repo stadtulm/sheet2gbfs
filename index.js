ab7b15b1f2a4f306b76951ef4f68c9df9dd7d904
Maximilian Richt <maxi@richt.name>
2019-07-24 19:27:45 +0200

    get single sheet urls from api, async/await

--- index.js
+++ index.js
@@ -1,52 +1,70 @@
 GBFS_ENDPOINT = process.env.GBFS_ENDPOINT || "http://gbfs.example.invalid";
-LANGUAGE = "de";
+LANGUAGE = process.env.LANGUAGE || "de";
 
 GOOGLE_SHEETS_KEY =
   process.env.GOOGLE_SHEETS_KEY ||
   "1OFBnUAaoh3shRO1ZArFxYpad-Xml7dyXgFfrchq0-Uo";
-GOOGLE_SHEETS_BIKE_URL = `https://spreadsheets.google.com/feeds/list/${GOOGLE_SHEETS_KEY}/2/public/full?alt=json`;
-GOOGLE_SHEETS_STATION_URL = `https://spreadsheets.google.com/feeds/list/${GOOGLE_SHEETS_KEY}/1/public/full?alt=json`;
-GOOGLE_SHEETS_SYSTEM_URL = `https://spreadsheets.google.com/feeds/list/${GOOGLE_SHEETS_KEY}/3/public/full?alt=json`;
+GOOGLE_SHEETS_URL = `https://spreadsheets.google.com/feeds/worksheets/${GOOGLE_SHEETS_KEY}/public/full?alt=json`;
 
 const fs = require("fs");
 const fetch = require("node-fetch");
-
-writeIndex();
-loadSystemInfo();
-loadStationInfo();
 
 let station_status = {
   stations: []
 };
 
-function writeIndex() {
+function fetchSheets() {
+  let sheets = {};
+  return fetch(GOOGLE_SHEETS_URL)
+    .then(res => res.json())
+    .then(json => {
+      json.feed.entry.forEach(sheet => {
+        let title = sheet["title"]["$t"].toLowerCase();
+        let link = sheet["link"].find(
+          obj =>
+            obj["rel"] == "http://schemas.google.com/spreadsheets/2006#listfeed"
+        );
+        if (link) {
+          let url = link["href"] + "?alt=json";
+          sheets[title] = url;
+        }
+      });
+      return sheets;
+    });
+}
+
+function writeIndex(sheets) {
   let gbfs_json = {
     last_updated: Math.floor(new Date() / 1000),
     ttl: 0,
     data: {}
   };
-  gbfs_json["data"][LANGUAGE] = {
-    feeds: [
-      {
-        name: "system_information",
-        url: GBFS_ENDPOINT + "/system_information.json"
-      },
-      {
-        name: "station_information",
-        url: GBFS_ENDPOINT + "/station_information.json"
-      },
-      {
-        name: "station_status",
-        url: GBFS_ENDPOINT + "/station_status.json"
-      }
-    ]
-  };
+  gbfs_json["data"][LANGUAGE] = { feeds: [] };
+  if (sheets["system"]) {
+    gbfs_json["data"][LANGUAGE]["feeds"].push({
+      name: "system_information",
+      url: GBFS_ENDPOINT + "/system_information.json"
+    });
+  }
+  if (sheets["stations"]) {
+    gbfs_json["data"][LANGUAGE]["feeds"].push({
+      name: "station_information",
+      url: GBFS_ENDPOINT + "/station_information.json"
+    });
+  }
+  if (sheets["bikes"]) {
+    gbfs_json["data"][LANGUAGE]["feeds"].push({
+      name: "station_status",
+      url: GBFS_ENDPOINT + "/station_status.json"
+    });
+  }
   fs.writeFileSync("gbfs/gbfs.json", JSON.stringify(gbfs_json));
 }
 
-function loadSystemInfo() {
-  fetch(GOOGLE_SHEETS_SYSTEM_URL).then(res => {
-    res.json().then(json => {
+function loadSystemInfo(url) {
+  return fetch(url)
+    .then(res => res.json())
+    .then(json => {
       let data = json.feed.entry[0];
       let systemInformation = {
         last_updated: Math.floor(new Date() / 1000),
@@ -65,15 +83,15 @@ function loadSystemInfo() {
         JSON.stringify(systemInformation)
       );
     });
-  });
 }
 
-function loadStationInfo() {
+function loadStationInfo(url) {
   let station_information = {
     stations: []
   };
-  fetch(GOOGLE_SHEETS_STATION_URL).then(res => {
-    res.json().then(json => {
+  return fetch(url)
+    .then(res => res.json())
+    .then(json => {
       json.feed.entry.forEach(station => {
         let gbfsStation = {
           station_id: station["gsx$stationid"]["$t"],
@@ -102,14 +120,13 @@ function loadStationInfo() {
         "gbfs/station_information.json",
         JSON.stringify(station_information)
       );
-      loadBikeInfo();
     });
-  });
 }
 
-function loadBikeInfo() {
-  fetch(GOOGLE_SHEETS_BIKE_URL).then(res => {
-    res.json().then(json => {
+function loadBikeInfo(url) {
+  return fetch(url)
+    .then(res => res.json())
+    .then(json => {
       json.feed.entry.forEach(bike => {
         if (bike["gsx$status"]["$t"] == "AVAILABLE") {
           let station = bike["gsx$station"]["$t"];
@@ -126,5 +143,30 @@ function loadBikeInfo() {
         JSON.stringify(station_status)
       );
     });
-  });
 }
+
+(async function() {
+  var sheets = await fetchSheets();
+  await writeIndex(sheets);
+  if (typeof sheets["system"] === "undefined") {
+    console.warn(
+      "system sheet is missing. not going to write system_information.json"
+    );
+  } else {
+    await loadSystemInfo(sheets["system"]);
+  }
+  if (typeof sheets["stations"] === "undefined") {
+    console.warn(
+      "station sheet is missing. not going to write station_information.json"
+    );
+  } else {
+    await loadStationInfo(sheets["stations"]);
+  }
+  if (typeof sheets["bikes"] === "undefined") {
+    console.warn(
+      "bike sheet is missing. not going to write station_status.json"
+    );
+  } else {
+    await loadBikeInfo(sheets["bikes"]);
+  }
+})();
